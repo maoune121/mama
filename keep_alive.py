0bbecbaa8a4850f6b0417a4540ff54e7ad7b4c5a
maoune121 <maoune10002@gmail.com>
2025-03-18 16:57:19 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from threading import Thread
-import logging
 
-app = Flask('')
-# Suppress Flask's default logging messages
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "✅ Bot is running!"
 
 def run():
-    app.run(host="0.0.0.0", port=5000)  # Changed port from 8080 to 5000
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
